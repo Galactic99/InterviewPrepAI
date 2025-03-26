d08d1ec989c4427508e5759bfd966b8bea9604cd
Galactic99 <abhinavashok2004@gmail.com>
2025-03-26 20:04:34 +0530

    reordering hero

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,8 +8,9 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
     <div className="root-layout">
       <nav className="flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
-          <div>
+          <div className="flex gap-2">
             <Image src="/logo.svg" alt="logo" height={32} width={38} />
+            <h2>PrepWise</h2>
           </div>
         </Link>
         <div>
@@ -22,40 +23,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
           />
         </div>
       </nav>
-
-      <div className="bg-gradient-to-b from-[#171532] to-[#08090D] rounded-2xl">
-        <div className="container mx-auto px-4 py-8 flex justify-between items-start">
-          <div className="max-w-xl pt-8 pl-8">
-            <p className="text-4xl font-bold text-white">
-              Get Interview Ready with Ai-
-            </p>
-            <p className="text-4xl font-bold text-white mb-4">
-              Powered Pracitce & Feedback
-            </p>
-            <p>
-              Practice real interview questions & get instant feedback
-            </p>
-            <Button className="mt-6 rounded-full font-semibold bg-[#CAC5FE] hover:bg-[#B8B2FF] text-black">
-              Start an Interview
-            </Button>
-          </div>
-          <div className="flex-shrink-0">
-            <Image
-              src="/robot.png"
-              alt="robot"
-              width={441}
-              height={322}
-              className="object-contain"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <div>
-          <h3>Your Past Interviews</h3>
-        </div>
-      </div>
+      {children}
     </div>
   );
 };
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 const Page = () => {
   return (
-    <div> Home Page</div>
+    <>
+         <div className="bg-gradient-to-b from-[#171532] to-[#08090D] rounded-2xl">
+        <div className="container mx-auto px-4 py-8 flex justify-between items-start">
+          <div className="max-w-xl pt-8 pl-8">
+            <p className="text-4xl font-bold text-white">
+              Get Interview Ready with Ai-
+            </p>
+            <p className="text-4xl font-bold text-white mb-4">
+              Powered Pracitce & Feedback
+            </p>
+            <p>Practice real interview questions & get instant feedback</p>
+            <Button className="mt-6 rounded-full font-semibold bg-[#CAC5FE] hover:bg-[#B8B2FF] text-black">
+              Start an Interview
+            </Button>
+          </div>
+          <div className="flex-shrink-0">
+            <Image
+              src="/robot.png"
+              alt="robot"
+              width={441}
+              height={322}
+              className="object-contain"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h3>Your Past Interviews</h3>
+        </div>
+      </div>
+    </>
   )
 }
 
